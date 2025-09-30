cc72858df850638154e0b7dc2c1261cdd81dbf53
Wadad Parker <wadadparker5@gmail.com>
2025-09-30 19:32:52 +0530

    feat: tailwind css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+
+import "./index.css"
+
 const rootElement = document.getElementById('root');
 <p>hello world</p>
 if (rootElement) {
