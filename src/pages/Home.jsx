46c1e08ee2fd85f064c4f6b1c68103bf34e48662
Laraib ahmmad <labbuxt@gmail.com>
2025-10-02 19:17:53 +0530

    feat:updated drawer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div>
-      
+    <div className='flex justify-center text-3xl h-20 font-bold'>
+      <p>The Trend Lies Here</p>
     </div>
   )
 }
