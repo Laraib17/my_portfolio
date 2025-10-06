fc06e9712d0c6c4938d38a3d9a47f074ae4cecb4
Laraib ahmmad <labbuxt@gmail.com>
2025-10-07 00:25:39 +0530

    refactor: cleaned up project files and imported latest images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Product from './pages/Product';
 import Login from './pages/Login';
-import PlaceOrder from './pages/PlaceOrder';
-import Order from './pages/Order';
-import Cart from './pages/cart';
 import Contact from './pages/Contact';
 import BasicExample from './components/Navbar';
 
@@ -15,11 +11,7 @@ const App = () => {
       <BasicExample/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/product/:productId" element={<Product />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/placeorder" element={<PlaceOrder />} />
-        <Route path="/order" element={<Order />} />
-        <Route path="/cart" element={<Cart />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {firstImg} from "../assets/assets";
+//import { secondImg } from '../assets/assets';
 const home = () => {
   return (
     <div className='flex flex-col items-center mt-10'>
       <p className='p-4 text-3xl h-20 font-bold'>The Trend Lies Here</p>
-      <div className=' bg-blue-100 w-11/12 rounded-lg h-60'>
+      <div className=' bg-blue-100 rounded-b-full w-11/12 rounded-lg h-60'>
         <div className=' bg-amber-300 flex flex-col items-center p-4'>
           <div className='flex flex-row justify-between gap-3'>
             <p>Left content</p>
@@ -11,12 +13,14 @@ const home = () => {
           </div>
         </div>
       </div>
-      <div className='flex flex-row w-1/2 items-center mt-10 bg-blue-100 h-60'>
-        <div className='bg-green-200'>
-          <p>Half left block</p>
-        </div>
-        <div className='bg-red-200'>
-          <p>Half right block</p>
+      <div className='flex flex-row rounded-t-full w-11/12 mt-10 bg-blue-100 h-60'>
+          <p className='bg-green-100'>Half left block</p>
+          <p className='bg-red-100'>Half right block</p>
+      </div>
+      <br />
+      <div>
+        <div>
+          <img src={firstImg} alt="" />
         </div>
       </div>
     </div>
