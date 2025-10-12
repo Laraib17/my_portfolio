15e195b0202432d118239491baeee9f558130bd4
Laraib ahmmad <labbuxt@gmail.com>
2025-10-12 23:41:44 +0530

    feat:updated login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -22,5 +22,28 @@ const login = () => {
     </div>
   );
 }
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+      <div>
+        <section>
+          <ul></ul>
+            <li>Name: John Doe</li>
+            <li>Email: john.doe@example.com</li>
+            <li>Message: Hello, I would like to know more about your services.</li>
+          </ul>
+        </section>
+        <section></section>
+        <section></section>
+        <section></section>
+      </div> 
+    </div>
+  );
+}
+
+export default contact
+
 
 export default login
