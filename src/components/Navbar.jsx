7d4380ce9b0ef79dc4a4822611cd151694a792fa
Laraib ahmmad <labbuxt@gmail.com>
2025-10-15 23:41:11 +0530

    feat: navbar upgradation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { logo } from "../assets/assets";
 import {profile_icon} from "../assets/assets";
 function BasicExample() {
   return (
-    <div className="flex items-center justify-between sm:items-center px-10 py-4 bg-gray-800  text-teal-200">
+    <div className="flex items-center justify-between sm:items-center px-10 py-4 bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 text-white">
       <div className="flex items-center gap-2 justify-between">
          <button onClick={() => document.getElementById("my-drawer-3").checked = true} className="drawer-toggle btn btn-primary lg:hidden">
             =
@@ -13,23 +13,23 @@ function BasicExample() {
       <ul className="flex gap-8 flex-row text-md font-medium">
         <NavLink to="/" className="flex flex-col items-center gap- ">
           <p className="hover:text-teal-500 ">Home</p>
-          <hr className="border-t border-teal-500 w-full hidden" />
+          <hr className="border-t border-ptext-teal-500 w-full hidden" />
         </NavLink>
         <NavLink to="/about" className="flex flex-col items-center gap- ">
           <p className="hover:text-teal-500">About</p>
-          <hr className="border-t border-teal-500 w-full hidden" />
+          <hr className="border-t border-ptext-teal-500 w-full hidden" />
         </NavLink>
         <NavLink to="/services" className="flex flex-col items-center gap- ">
           <p className="hover:text-teal-500">Services</p>
-          <hr className="border-t border-teal-500 w-full hidden" />
+          <hr className="border-t border-ptext-teal-500 w-full hidden" />
         </NavLink>
         <NavLink to="/contact" className="flex flex-col items-center gap- ">
           <p className="hover:text-teal-500">Contact</p>
-          <hr className="border-t border-teal-500 w-full hidden" />
+          <hr className="border-t border-ptext-teal-500 w-full hidden" />
         </NavLink>
       </ul>
       <div className="flex items-center gap-4">
-        <button className="bg-teal-500 text-white px-4 py-2 rounded-full hover:bg-teal-600">
+        <button className="bg-purpl text-white px-4 py-2 rounded-full hover:bg-purpl">
           Login
         </button>
         <div className="group relative">
