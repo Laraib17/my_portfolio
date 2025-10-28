c15cf6727b0fdbd6bc8703111612b4c684414729
Laraib Ahmad <112575218+Laraib17@users.noreply.github.com>
2025-10-28 15:05:19 +0530

    refactor: migrate data fetching to React Query

--- main.jsx
+++ main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import {QueryClient,quryClientProvider} from './queryClient';
 import './index.css';
-
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </HashRouter>
   </React.StrictMode>
 );
