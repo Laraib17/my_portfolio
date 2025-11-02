a98ead6c7ef4ac3f09b3ba1ac588638d6ba98a05
Laraib Ahmad <112575218+Laraib17@users.noreply.github.com>
2025-11-02 22:17:57 +0530

    feat:added fetching data

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -2,4 +2,5 @@ import firstImg from './first.jpg';
 import secondImg from './second.jpg';
 import logo from './logo.png';
 import profile_icon from './profile_icon.png';
-export { firstImg, secondImg, logo, profile_icon };
+import infinite_spinner from './infinite_spinner.svg';
+export { firstImg, secondImg, logo, profile_icon ,infinite_spinner};
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { UseQuery } from '@tanstack/react-query';
+import {infinite_spinner} from "../assets/assets";
 const get_projects = async (url) => {
   const res = await fetch(url);
   if (!res.ok) throw new Error('Network response was not ok');
@@ -8,11 +9,11 @@ const get_projects = async (url) => {
 const projects = () => {
   const {data,isPending,error}= UseQuery({
     queryKey: ['projects'],
-    queryFn: () => get_projects('/api/projects')
+    queryFn: () => get_projects
   })
   return (
-    <div>
-
+    <div className='flex'>
+      {isPending? <img src={spinner} alt="Loading..." /> : console.error("Error occured")}
     </div>
   )
 }
