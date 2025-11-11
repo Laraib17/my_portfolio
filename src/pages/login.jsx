72ae59c53ff5812cbb19bce0f84395da77fedaa4
Laraib Ahmad <112575218+Laraib17@users.noreply.github.com>
2025-11-11 23:38:45 +0530

    feat:edited login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -9,11 +9,11 @@ const login = () => {
           <button className='bg-blue-500 text-white px-4 py-2 rounded'>send data </button></div>
         </section>
         <section>
-          <div className="flex border-b "><h1 className='text-xl font-bold'>hello</h1>
+          <div className="flex border-b "><h1 className='text-xl font-bold'>again</h1>
           <p>over</p></div>
         </section>
         <section>
-          <div className="flex border-b "><h1 className='text-xl font-bold'>hello</h1>
+          <div className="flex border-b "><h1 className='text-xl font-bold'>from my side</h1>
           <p>world</p></div>
         </section>
       </div> 
