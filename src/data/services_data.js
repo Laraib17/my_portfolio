5750570d7e6b967fd099339089df94bcf3399e39
Laraib Ahmad <112575218+Laraib17@users.noreply.github.com>
2025-11-30 23:43:35 +0530

    feat:Services page updated

---
+++ src/data/services_data.js
@@ -0,0 +1,26 @@
+export const service = [
+    {
+      id:"1",
+      title:"Professional Dynamic Website",
+      description:"",
+      icon:"",
+    },
+    {
+      id:"2",
+      title:"Flutter Apps",
+      description:"",
+      icon:"",
+    },
+    {
+      id:"3",
+      title:"ML-Models",
+      description:"",
+      icon:"",
+    },
+    {
+      id:"4",
+      title:"",
+      description:"",
+      icon:"",
+    }
+]
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,29 +1,20 @@
 import React from "react";
+import { service } from "../data/services_data";
 const Services = () => {
   return (
     <div>
       <div>
-        <section>
-          <div className="flex border-b">
-            <h1 className="text-xl font-bold">hello</h1>
-            <p>AAkhirah</p>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded">
-              send data{" "}
-            </button>
-          </div>
-        </section>
-        <section>
-          <div className="flex border-b ">
-            <h1 className="text-xl font-bold">again</h1>
-            <p>over</p>
-          </div>
-        </section>
-        <section>
-          <div className="flex border-b ">
-            <h1 className="text-xl font-bold">from my side</h1>
-            <p>world</p>
-          </div>
-        </section>
+        {service.map((service) => (
+          <section key={service.id}>
+            <div className="flex border-b">
+              <h1 className="text-xl font-bold">{service.title}</h1>
+              <p>{service.description}</p>
+              <button className="bg-blue-500 text-white px-4 py-2 rounded">
+                send data{" "}
+              </button>
+            </div>
+          </section>
+        ))}
       </div>
     </div>
   );
