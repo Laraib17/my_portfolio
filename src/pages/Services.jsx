ca351452656cbc9637d9e733ffc28b0ae9914b20
Laraib Ahmad <112575218+Laraib17@users.noreply.github.com>
2025-12-03 23:46:07 +0530

    feat:edits

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -21,7 +21,7 @@ const Services = () => {
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
-            className="text-4xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-600 mb-4"
+            className="text-4xl md:text-5xl font -bold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-600 mb-4"
           >
             My Services
           </motion.h2>
