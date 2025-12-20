ca10fca7ddf034e9db31f6e18a500f08993c60eb
Laraib Ahmad <112575218+Laraib17@users.noreply.github.com>
2025-12-20 23:56:16 +0530

    feat:project added

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: "CryptoForecast-ML ",
     description: "Built an AI/ML system to predict next-day cryptocurrency prices across 25 digital assets Engineered 70+ technical indicators and time-series features from 5 years of market data Trained and compared 8 ML algorithms (Random Forest, XGBoost, LightGBM); achieved R2 > 0.95",
-    tech: ["Python", "Machine Learning", "Data Analysis", "Data Visualization"],
+    tech: ["Python", "Machine Learning", "Data Analysis", "Data Visualization","Machine Learning","Data Analysis","Data Visualization"],
     github: "https://github.com/Laraib17/crypto_price_prediction" // Placeholder
   },
   {
