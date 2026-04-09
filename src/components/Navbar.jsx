2c8bcb0215b6c799097ae66eef7880b3f89794b1
Laraib Ahmad <112575218+Laraib17@users.noreply.github.com>
2026-04-09 23:10:11 +0530

    feat:resolves

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
-import { logo } from "../assets/assets";
-import { profile_icon } from "../assets/assets";
+// import { logo } from "../assets";
+// import { profile_icon } from "../assets";
 const BasicExample = () => {
   return (
     <div className="flex items-center justify-between sm:items-center px-10 py-4 bg-linear-to-br from-slate-900 via-purple-900 to-slate-900 text-white">
@@ -13,7 +13,7 @@ const BasicExample = () => {
         >
           =
         </button> */}
-        <img src={logo} alt="Logo" className="h-8 w-10 rounded-full hover:border hover: size-2.5 border-purple-500" />
+        {/* <img src={logo} alt="Logo" className="h-8 w-10 rounded-full hover:border hover: size-2.5 border-purple-500" /> */}
       </div>
       <ul className="flex gap-8 flex-row text-md font-medium">
         <NavLink to="/" className="flex flex-col items-center gap- ">
@@ -38,11 +38,11 @@ const BasicExample = () => {
           Coffee
         </button>
         <div className="group relative">
-          <img
+          {/* <img
             className="w-10 cursor-pointer rounded-full"
             src={profile_icon}
             alt=""
-          />
+          /> */}
           <div className="group-hover:block hidden absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded shadow-lg">
             <div className="p-4 border-b border-gray-200"></div>
           </div>
