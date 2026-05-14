9e6b505b28f494a816cf15b0c474558f140429e2
Laraib Ahmad <112575218+Laraib17@users.noreply.github.com>
2026-05-14 23:43:51 +0530

    feat:new edits

--- src/pages/get_service.jsx
+++ src/pages/get_service.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import PurpleBubbles from "../components/PurpleBubbles";
 const get_service = () => {
   const services=[]
   return (
-    <div classname="flex flex-col items-center justify-center">
+    <div classname="flex flex-col items-center justify-center gradient-to-r from-blue-500 to-purple-500 min-h-screen">
+      <PurpleBubbles />
       <div>
-        
+
       </div>
     </div>
   )
