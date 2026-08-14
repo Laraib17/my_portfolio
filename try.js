041011b00355e591b2096adfd85e13422b3ac640
Laraib <Labbuxt@gmail.com>
2026-08-14 23:59:17 +0530

    feat:verbosee edit

--- try.js
+++ try.js
@@ -1,5 +1,5 @@
-import {url,log} from "./math.js"
+import {url,log,add} from "./math.js"
 const m=require("./math.js")
 function name(name) {
-    console.log("Hello, " + name + "!");
+    console.log("Hello, " + name + "!"+"if you add 2 and 3, you will get "+m.add(2,3));
 }
